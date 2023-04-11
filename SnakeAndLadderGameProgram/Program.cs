3c752183a77debe460745c3d64651db9b0c0c909
Sushil <skshinde2028@gmail.com>
2023-04-11 20:54:30 +0530

    [Sushil] adding rolling a dice code

--- SnakeAndLadderGameProgram/Program.cs
+++ SnakeAndLadderGameProgram/Program.cs
@@ -11,8 +11,15 @@ namespace SnakeAndLadderGameProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and ladder program");
+            //UC2
             int START_POSITION = 0;
             Console.WriteLine("Start position of player is: " + START_POSITION);
+            Console.WriteLine("Roll a dice");
+            Console.ReadLine();
+            Random random = new Random();
+            int countDiceRoll = random.Next(1, 7);
+            Console.WriteLine(countDiceRoll);
+            Console.ReadLine();
         }
     }
 }
