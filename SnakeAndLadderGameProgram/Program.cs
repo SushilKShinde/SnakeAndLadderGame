e1479428619bc96cbb9b1bdd2256a40e807f2523
Sushil <skshinde2028@gmail.com>
2023-04-12 21:59:19 +0530

    [Sushil] Counting how much times the dice is rolled to win

--- SnakeAndLadderGameProgram/Program.cs
+++ SnakeAndLadderGameProgram/Program.cs
@@ -17,42 +17,57 @@ namespace SnakeAndLadderGameProgram
             int initialPosition = 0;
             int finalPosition = 100;
             int playerPosition = initialPosition;
+            int DieCount = 0;
             Console.WriteLine("Welcome To Snake Ladder Game!");
             while (playerPosition < finalPosition)
             {
+                DieCount++;
                 Random random = new Random();
                 int DieNumber = random.Next(1, 7);
                 Console.WriteLine("DieNumber : " + DieNumber);
-                //UC4 Setting Options For the Game(NoPlay, Ladd);
+                //UC3 Setting Options For the Game(NoPlay, Ladd);
                 Random random1 = new Random();
-                int playerOption = random1.Next(0, 3);
+                int playerOption = random1.Next(0, 4);
                 const int SNAKE = 1;
                 const int LADDER = 2;
+                const int noPlay = 0;
                 Console.WriteLine("PlayerOption : " + playerOption);
+                //Console.WriteLine("playerPosition : " + playerPosition);
                 switch (playerOption)
                 {
                     case LADDER:
                         playerPosition += DieNumber; // If ladder comes player position will be incremented;
-                        if (playerPosition > finalPosition)
+                        if (playerPosition > finalPosition) // This If condition executed When ever player position More than 100 and sets previous value if the die number is more than required number
                         {
-                            playerPosition -= DieNumber;
+                            playerPosition -= DieNumber; // seting player position to previous position
                         }
                         break;
                     case SNAKE:
                         playerPosition -= DieNumber; // If Snake comes player position will be decremented;
-                        if (playerPosition < initialPosition)
+                        if (playerPosition < initialPosition) // This If condition executed When ever player position less than 0 and sets Value player initial value zero;
                         {
                             playerPosition = initialPosition;
                         }
+
                         break;
-                    default:
+                    case noPlay:
                         playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
+                        break;
+
+                    default:
+                        playerPosition += DieNumber;
+                        if (playerPosition > finalPosition) // This If condition executed When ever player position More than 100 and sets previous value if the die number is more than required number
+                        {
+                            playerPosition -= DieNumber; // seting player position to previous position
+                        }
+
                         break;
                 }
                 Console.WriteLine("playerPosition : " + playerPosition);
 
             }
-            Console.WriteLine("Player Final Position : " + playerPosition);
+            Console.WriteLine("DieCount : " + DieCount);
+            Console.WriteLine("Player Final Position : " + playerPosition + " And Die Rolled For " + DieCount + " Times");
         }
     }
 }
