65235a0429ef8ba54bc136f21692f022aaf7d241
Sushil <skshinde2028@gmail.com>
2023-04-12 21:38:20 +0530

    [Sushil] checking for the options

--- SnakeAndLadderGameProgram/Program.cs
+++ SnakeAndLadderGameProgram/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace SnakeAndLadderGameProgram
 {
@@ -10,16 +13,33 @@ namespace SnakeAndLadderGameProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Snake and ladder program");
-            //UC2
-            int START_POSITION = 0;
-            Console.WriteLine("Start position of player is: " + START_POSITION);
-            Console.WriteLine("Roll a dice");
-            Console.ReadLine();
+            Console.WriteLine("Welcome To Snake Ladder Game!");
+            int initialPosition = 0;
+            int finalPosition = 100;
+            int playerPosition = initialPosition;
             Random random = new Random();
-            int countDiceRoll = random.Next(1, 7);
-            Console.WriteLine(countDiceRoll);
-            Console.ReadLine();
+            int DieNumber = random.Next(1, 7);
+            Console.WriteLine(DieNumber);
+            //UC3 Setting Options For the Game(NoPlay, Ladd);
+            Random random1 = new Random();
+            int playerOption = random1.Next(0, 3);
+            const int SNAKE = 1;
+            const int LADDER = 2;
+            Console.WriteLine(playerOption);
+            Console.WriteLine(playerPosition);
+            switch (playerOption)
+            {
+                case LADDER:
+                    playerPosition += DieNumber; // If ladder comes player position will be incremented;
+                    break;
+                case SNAKE:
+                    playerPosition -= DieNumber; // If Snake comes player position will be decremented;
+                    break;
+                default:
+                    playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
+                    break;
+            }
+            Console.WriteLine(playerPosition);
         }
     }
 }
