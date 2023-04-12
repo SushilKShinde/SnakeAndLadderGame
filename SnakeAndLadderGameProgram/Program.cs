db86d4bfd0b1355bc15c3832211904dd2a4be8e3
Sushil <skshinde2028@gmail.com>
2023-04-12 23:16:23 +0530

    [Sushil] Modifying UC6

--- SnakeAndLadderGameProgram/Program.cs
+++ SnakeAndLadderGameProgram/Program.cs
@@ -13,7 +13,7 @@ namespace SnakeAndLadderGameProgram
     {
         static void Main(string[] args)
         {
-            //UC1 Initilizing initialPosition,finalPosition,playerInitialPosition
+            //UC6
             int initialPosition = 0;
             int finalPosition = 100;
             int playerPosition = initialPosition;
@@ -25,46 +25,33 @@ namespace SnakeAndLadderGameProgram
                 Random random = new Random();
                 int DieNumber = random.Next(1, 7);
                 Console.WriteLine("DieNumber : " + DieNumber);
-                //UC3 Setting Options For the Game(NoPlay, Ladd);
                 Random random1 = new Random();
                 int playerOption = random1.Next(0, 4);
                 const int SNAKE = 1;
                 const int LADDER = 2;
                 const int noPlay = 0;
                 Console.WriteLine("PlayerOption : " + playerOption);
-                //Console.WriteLine("playerPosition : " + playerPosition);
-                switch (playerOption)
+                if (playerOption == SNAKE)
                 {
-                    case LADDER:
-                        playerPosition += DieNumber; // If ladder comes player position will be incremented;
-                        if (playerPosition > finalPosition) // This If condition executed When ever player position More than 100 and sets previous value if the die number is more than required number
-                        {
-                            playerPosition -= DieNumber; // seting player position to previous position
-                        }
-                        break;
-                    case SNAKE:
-                        playerPosition -= DieNumber; // If Snake comes player position will be decremented;
-                        if (playerPosition < initialPosition) // This If condition executed When ever player position less than 0 and sets Value player initial value zero;
-                        {
-                            playerPosition = initialPosition;
-                        }
-
-                        break;
-                    case noPlay:
-                        playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
-                        break;
-
-                    default:
-                        playerPosition += DieNumber;
-                        if (playerPosition > finalPosition) // This If condition executed When ever player position More than 100 and sets previous value if the die number is more than required number
-                        {
-                            playerPosition -= DieNumber; // seting player position to previous position
-                        }
-
-                        break;
+                    playerPosition = playerPosition - DieNumber; ;
+                    if (playerPosition < initialPosition)
+                    {
+                        playerPosition = initialPosition;
+                    }
+                }
+                else if (playerOption == LADDER)
+                {
+                    playerPosition = playerPosition + DieNumber; 
+                    if (playerPosition > finalPosition) 
+                    {
+                        playerPosition = playerPosition - DieNumber; 
+                    }
+                }
+                else
+                {
+                    playerPosition = playerPosition;
                 }
                 Console.WriteLine("playerPosition : " + playerPosition);
-
             }
             Console.WriteLine("DieCount : " + DieCount);
             Console.WriteLine("Player Final Position : " + playerPosition + " And Die Rolled For " + DieCount + " Times");
